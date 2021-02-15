437097118465ba4bf0576458be798704f27ef74d
Alvaro Bartolome <alvarob96@usal.es>
2021-02-15 21:24:21 +0100

    prediction2label function & handled tf serving request

--- src/streamlit/ui.py
+++ src/streamlit/ui.py
@@ -29,42 +29,17 @@ else:
     st.image(image_as_bytes, use_column_width=True)
     pred_button = st.button("Predict")
 
+# Converts the input image into a Tensor
+image_tensor = image2tensor(image_as_bytes=image_as_bytes)
 
-# def process(image, server_url: str):
+# Prepare the data that is going to be sent in the POST request
+json_data = {
+  "instances": image_tensor
+}
 
-#     m = MultipartEncoder(fields={"file": ("filename", image, "image/jpeg")})
+# Send the request to the Prediction API
+response = requests.post(REST_URL, json=json_data)
 
-#     r = requests.post(
-#         server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000
-#     )
-
-#     return r
-
-
-# # construct UI layout
-# st.title("DeepLabV3 image segmentation")
-
-# st.write(
-#     """Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
-#          This streamlit example uses a FastAPI service as backend.
-#          Visit this URL at `:8000/docs` for FastAPI documentation."""
-# )  # description and instructions
-
-# input_image = st.file_uploader("insert image")  # image upload widget
-
-# if st.button("Get segmentation map"):
-
-#     col1, col2 = st.beta_columns(2)
-
-#     if input_image:
-#         segments = process(input_image, backend)
-#         original_image = Image.open(input_image).convert("RGB")
-#         segmented_image = Image.open(io.BytesIO(segments.content)).convert("RGB")
-#         col1.header("Original")
-#         col1.image(original_image, use_column_width=True)
-#         col2.header("Segmented")
-#         col2.image(segmented_image, use_column_width=True)
-
-#     else:
-#         # handle case with no image
-#         st.write("Insert an image!")
+# Retrieve the highest probablity index of the Tensor (actual prediction)
+prediction = response.json()['predictions'][0]
+label = prediction2label(prediction=prediction)
--- src/streamlit/utils.py
+++ src/streamlit/utils.py
@@ -3,6 +3,8 @@
 
 import tensorflow as tf
 
+from constants import MAPPING
+
 def image2tensor(image_as_bytes):
     """
     Receives a image as bytes as input, that will be loaded,
@@ -19,3 +21,15 @@ def image2tensor(image_as_bytes):
     image = tf.expand_dims(image, 0)
     image = image.numpy().tolist()
     return image
+
+
+def prediction2label(prediction):
+    """
+    Receives the prediction Tensor and retrieves the index with the highest
+    probability, so as to then map the index value in order to get the
+    predicted label. 
+    """
+
+    # Retrieve the highest probablity index of the Tensor (actual prediction)
+    prediction = tf.argmax(prediction)
+    return MAPPING[prediction.numpy()])
